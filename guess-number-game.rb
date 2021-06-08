12efe2d3ffd75b6d29415abf4379d327d5e5d38a
lcfz86 <zerolcfz86@gmail.com>
2021-06-08 17:07:08 +0800

    Add Updated Guess Number Game

--- guess-number-game.rb
+++ guess-number-game.rb
@@ -1,19 +1,43 @@
-puts 'Greetings, What is your name?: '
-name = gets.chomp
+player_score = []
 
-puts "Morning, #{name}"
+loop do
+  tries = 0
 
-puts "Please guess the number from 1 to 50."
-puts "May I know what the number you guessed is?: "
-number = gets.chomp.to_i
+  puts 'Greetings, What is your name?: '
+  name = gets.chomp
 
-guess = rand 1..50
+  player_score << [name, tries]
 
-while number != guess
-  puts 'Wrong number, Please guess again!'
+  puts "Morning, #{name}"
+
+  puts "Please guess the number from 1 to 50."
+  puts "May I know what the number you guessed is?: "
   number = gets.chomp.to_i
-end
 
-if number == guess
-  puts 'Congratulation, your number is correct!'
+  guess = rand 1..50
+
+  tries += 1
+
+  while number != guess
+    if number > guess
+      puts 'Wrong number, go lower.'
+    else
+      puts 'Wrong number, go higher.'
+    end
+    number = gets.chomp.to_i
+    tries += 1
+  end
+
+  if number == guess
+    player_score.last[1] = tries
+    puts player_score.inspect
+    puts 'Congratulation, your number is correct!'
+
+    # Print out the scores
+    puts 'Scores'
+    puts '=' * 50
+    player_score.each do |player|
+      puts "Name: #{player[0]} - Score: #{player[1]}"
+    end
+  end
 end
